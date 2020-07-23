aa6e2928ebdbf30a372b0e1ba39ff8cd94084141
davidcwh <dcwh12321@gmail.com>
2020-07-23 14:44:01 +0800

    Add favicon.ico

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
+import favicon from '../media/favicon.ico';
+
 const Head = ({ title }) => {
     const data = useStaticQuery(graphql`
         query {
@@ -14,7 +16,9 @@ const Head = ({ title }) => {
     `)
 
     return (
-        <Helmet title={`${title} | ${data.site.siteMetadata.title}`}/>
+        <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+            <link rel="icon" href={favicon}/>
+        </Helmet>
     )
 }
 
